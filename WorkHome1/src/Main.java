1204775d1218fefbc924df9150f59d45b70d0eea
Alexander Shanov <shanov@pointcad.ru>
2023-01-23 22:16:48 +0300

    change in branch

--- WorkHome1/src/Main.java
+++ WorkHome1/src/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("Изменения для git");
         System.out.println("Please enter you full name(Введите полное имя)");
         String fullName = sc.nextLine();
+
         String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
         System.out.println("Program result(Результат): " + fullName);
 
