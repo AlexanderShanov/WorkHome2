7f6b0552439d9e984cf61ac1ed0e397f292ac945
Alexander Shanov <shanov@pointcad.ru>
2023-01-31 17:25:16 +0300

    second commint, it is first finish

--- WorkHome3/src/Main.java
+++ WorkHome3/src/Main.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int correctCount = 0;
+        int wrongCount = 0;
+
+        String[] questions = {
+                "Сколько букв сейчас в русском алфавите?",
+                "Сколько дней в январе?",
+                "Последний свет в радуге"
+        };
+
+        String[][] answerOptions = {
+                { "30", "32", "33"},
+                {"29", "30", "31"},
+                {"Белый", "Фиолетовый", "Синий"}
+        };
+
+        String[] correctAnswers = {"33","31", "Фиолетовый"};
+
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i = 0; i < answerOptions.length; i++){
+            System.out.println(questions[i]);
+            for (int j = 0; j < answerOptions.length; j++) {
+                System.out.println(String.valueOf(j + 1) + " " + answerOptions[i][j]);
+            }
+
+            System.out.print("Ваш ответ:");
+
+            if(scanner.hasNextInt()){
+                int numberAnswer = scanner.nextInt();
+                if(numberAnswer>0
+                        && numberAnswer<= correctAnswers.length
+                        && correctAnswers[i] == answerOptions[i][numberAnswer-1]){
+                    correctCount++;
+                }
+                else{
+                    wrongCount++;
+                }
+
+            }
+            else{
+                wrongCount++;
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("Результат: правильно " + correctCount + ", неправельно " + wrongCount);
+
     }
 }
